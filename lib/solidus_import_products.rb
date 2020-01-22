d990c8afc9422bb213c0c01d33fe85f03bbb6207
Nico Weling <nico.weling@googlemail.com>
2020-01-21 23:56:10 -0800

    Removed auth requirement in lib

--- lib/solidus_import_products.rb
+++ lib/solidus_import_products.rb
@@ -1,6 +1,6 @@
 require 'solidus_core'
 require 'solidus_support'
-require 'solidus_auth_devise'
+#require 'solidus_auth_devise'
 require 'deface'
 require 'solidus_import_products/exception'
 require 'solidus_import_products/logger'
